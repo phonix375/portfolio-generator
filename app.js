7fef6a3c656f36db408c4d940419ba3cdc67af48
Alexy Kotliar <kotliar.alex@gmail.com>
2021-04-12 22:48:54 -0400

    console log entered parametars

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const printProfileData = profileDataArr => {
-    for(let i = 0; i< profileDataArr.length;i++){
-        console.log(profileDataArr[i]);
-    }
+    profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+
 printProfileData(profileDataArgs);
