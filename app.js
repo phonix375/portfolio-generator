92c958d5614775437392dcd4dd5848e274558cdd
Alexy Kotliar <kotliar.alex@gmail.com>
2021-04-13 17:07:03 -0400

    export heml to file

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = (userName, githubName) => {
+    return`
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Protfolio Demo</title>
+</head>
+<body>
+    <H1>${userName}<H1>
+    <H2><a href='https://github.com/${githubName}'>GitHub</a></H2>
+</body>
+</html>
+    `};
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+    fs.writeFile('index.html',generatePage(name, github),err =>{
+        if(err) throw err;
 
-printProfileData(profileDataArgs);
+        console.log('Protfolio complete! check out index.html to see the ouput!');
+    } )
