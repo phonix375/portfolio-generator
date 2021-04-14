a26d958240bed44e1347daed8db99a3c935b1bda
Alexy Kotliar <kotliar.alex@gmail.com>
2021-04-13 23:45:14 -0400

    this is to update the page

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage  = () => 'name: Alex Kotliar, GitHub: Phonix375' ; 
 
-printProfileData(profileDataArgs);
+console.log(generatePage());
